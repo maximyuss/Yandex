a3928aa3cceca51964bafcad3830d567d907a31d
Maxim Saveliev <159418964+maximyuss@users.noreply.github.com>
2024-07-06 19:47:03 +0200

    Create 5-1h.py

---
+++ training-5.0/5-1h.py
@@ -0,0 +1,21 @@
+# https://contest.yandex.ru/contest/59539/problems/H/
+# Second version, clear solution
+
+l, x1, v1, x2, v2 = map(int, input().split())
+res = 10 ** 10
+for cycle in range(2):
+    dX = (x2 - x1 + l) % l
+    dV = v1 - v2
+    if dV < 0:
+        dV = -dV
+        dX = (-dX) % l
+    if dX == 0:
+        res = 0
+    if dV != 0:
+        res = min(res, dX / dV)
+    x2 = (-x2) % l
+    v2 = -v2
+if res == 10 ** 10:
+    print('NO')
+else:
+    print('YES\n', res)
